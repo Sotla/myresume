877745d641fc4389bf83ecec14438e595931690b
Panagiotis Angeloudis <p@ngl.gr>
2018-02-28 09:23:00 +0000

    clean

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-On WSL Bash:
+# PA-CV
 
-To initiate tlmgr (Tex Live package manager)
+Academic CVs end up being quite unmanagable after some point. This is a Python and Latex project for the automatic generation of my CV. Inspiration came from [James Keirstead](https://github.com/jkeirstead/jk-vita) and [Kusno Mudiarto](https://github.com/mudiarto/resume). The code used in this project was initially forked from the latter, but was modified to simplify and automate the build process, and allow for more streamlined template management.
 
+# Instructions
+
+While I normally use Windows, setting everything up there can get quite tedious. To generate my CV, I use an Ubuntu installation within the Windows Subsystem for Linux. This code assumes that you are operating with Python 3.x, although it should work with Python 2.x
+
+## setup instructions
+
+```
 sudo apt-get install texlive-latex-base texlive-latex-extra texlive-latex-recommended xzdec
+pip install cheetah3 tex pyyaml
 tlmgr init-usertree
 tlmgr option repository ftp://tug.org/historic/systems/texlive/2015/tlnet-final 
 tlmgr install biblatex logreq
+```
--- generate.py
+++ generate.py
@@ -8,15 +8,32 @@ templating engine
 from Cheetah.Template import Template
 from datetime import datetime
 from optparse import OptionParser
-import escapes
 import functools
 import os
 import sys
 import yaml
 import shutil
+import tex
+import re
 from shutil import copyfile
 import subprocess
 
+HYPERLINK_REGEX = re.compile(r'\[([^\]]*)\]\(([^)]*)\)')
+
+def escape_tex(s):
+    s = HYPERLINK_REGEX.sub(r'\href{\2}{\1}', s)
+    s = tex.escape_latex(s)
+    s = s.replace('LaTeX', r'\LaTeX')
+
+    # A bit of a hack... have to reverse some of tex.escape_latex escapes
+    s = s.replace('\\textbackslash{}', '\\')
+    s = s.replace('\\{', '{').replace('\\}', '}')
+    return s
+
+def escape_txt(s):
+    # Replace all markdown-esque hyperlinks with just the link text
+    s = HYPERLINK_REGEX.sub(r'\1', s)
+    return s
 
 def escape_leaves(escape_func, contents):
     """Escapes all leaves in the contents dict with escape_func"""
@@ -31,14 +48,15 @@ def escape_leaves(escape_func, contents):
 def get_cmd_line_args():
     parser = OptionParser(usage="%prog [options] <extension>",
         description="Fills in Cheetah templates from YAML source to " +
-                    "generate resume of the given <extension> format.")
+                    "generate resume of the given <extension> format."+
+                    "The default option is tex")
 
     parser.add_option("-o", "--output_dir",
         default='output',
         help="Output directory of generated files. Defaults to output/")
 
     parser.add_option("-s", "--source",
-        default='resume.yaml',
+        default='data/resume.yaml',
         help="Filename of the resume YAML source. Defaults to resume.yaml")
 
     parser.add_option("-t", "--template",
@@ -48,19 +66,17 @@ def get_cmd_line_args():
     options, extra_args = parser.parse_args()
     if len(extra_args) < 1:
         parser.print_help()
-        exit()
+        extra_args='tex'
+        
 
-    return options, extra_args[0]
+    return options, extra_args
 
 if __name__ == '__main__':
     options, extension = get_cmd_line_args()
-
     contents = yaml.load(open(options.source, 'r').read())
 
-    escape_func = getattr(escapes, 'escape_' + extension, lambda x: x)
-    escaped_contents = escape_leaves(escape_func, contents)
-    template = Template(file=options.template % extension,
-            searchList=[escaped_contents])
+    escaped_contents = escape_leaves(escape_tex, contents)
+    template = Template(file=options.template % extension, searchList=[escaped_contents])
 
     output_dir = options.output_dir
 
@@ -69,10 +85,18 @@ if __name__ == '__main__':
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    open('%s/resume.%s' % (output_dir, extension), 'w').write(str(template))
-    copyfile("Publications.bib","output/Publications.bib")
-    copyfile("res.cls","output/res.cls")
 
+
+
+    open('%s/resume.%s' % (output_dir, extension), 'w').write(str(template))
+    copyfile("data/publications.bib","output/publications.bib")
+    copyfile("templates/res.cls","output/res.cls")
+    
+    os.chdir("output")
+    os.system("pdflatex resume.tex")
+    os.system("bibtex resume")
+    os.system("pdflatex resume.tex")
+    os.system("pdflatex resume.tex")
     
 
 
